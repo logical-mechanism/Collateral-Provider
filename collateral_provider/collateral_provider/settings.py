38ec9989d0b48a58da267ee060ff8987d89a7905
logicalmechanism <logical.mechanism@protonmail.com>
2024-09-20 15:14:22 -0700

    adding templates back in

--- collateral_provider/collateral_provider/settings.py
+++ collateral_provider/collateral_provider/settings.py
@@ -64,6 +64,23 @@ ROOT_URLCONF = 'collateral_provider.urls'
 
 WSGI_APPLICATION = 'collateral_provider.wsgi.application'
 
+# TEMPLATES setting to support Django REST Framework's browsable API and any template rendering
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # Add custom template directories here
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
